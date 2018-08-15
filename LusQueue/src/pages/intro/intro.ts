cbe7b939a0d4619f56a6a8b01be64893656da9c1
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2018-08-15 17:12:27 -0300

    Add Style Intro

--- LusQueue/src/pages/intro/intro.ts
+++ LusQueue/src/pages/intro/intro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Tabs } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the IntroPage page.
@@ -15,13 +16,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) { //Estas informacoes se chamam injeção de dependencias
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
   }
 
-  
+  public goToTabsPage(){
+    this.navCtrl.push(TabsPage);//Usaremos a funcao do navCrtrl para fazer a navegacao, usando o metodo push passando como parametro a pagina para que queremos navegar
+
+  }
 
 }
