9ac104b26d106489f66e812b25b2b1a385342356
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2018-08-16 09:55:19 -0300

    Added Provider to incapsulate json informations

--- LusQueue/src/app/app.module.ts
+++ LusQueue/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { FeedPageModule } from '../pages/feed/feed.module';
 import { IntroPageModule } from '../pages/intro/intro.module';
+import { MoovieProvider } from '../providers/moovie/moovie';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MoovieProvider
   ]
 })
 export class AppModule {}
